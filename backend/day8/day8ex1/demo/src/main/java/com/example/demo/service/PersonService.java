72471c69c0e8df83800dec293ba1bf29673649d8
Mithunaa <151151235+Mithunaa106@users.noreply.github.com>
2024-02-29 15:14:05 +0530

    Add files via upload

---
+++ backend/day8/day8ex1/demo/src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,39 @@
+
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Person;
+import com.example.demo.repository.PersonRepo;
+
+@Service
+public class PersonService {
+    public PersonRepo personRepo;
+    public PersonService(PersonRepo personRepo)
+    {
+        this.personRepo = personRepo;
+    }
+
+    public boolean savePerson(Person person)
+    {
+        try
+        {
+            personRepo.save(person);
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+        return true;
+    }
+    public List<Person> getByStarting(String name)
+    {
+        return personRepo.findByPersonByStartingWith(name);
+    }
+    public List<Person> getByEnding(String name)
+    {
+        return personRepo.findByPersonByEndingWith(name);
+    }
+}
